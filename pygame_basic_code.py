4f404e9d5e3d0f0ee655ab7dd023fc18ad2d2f2f
erigra <erikgrande71@gmail.com>
2022-09-24 09:39:37 +0200

    b

--- pygame_basic_code.py
+++ pygame_basic_code.py
@@ -1,23 +1,29 @@
 import pygame
 import sys
 
+WIDTH, HEIGHT = 500, 500
+
 pygame.init()
-screen= pygame.display.set_mode(500,500)
+screen= pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Basic pygame mal")
 clock= pygame.time.Clock()
 gui_font= pygame.font.Font(None,30)
 
-background_color = (255,255,255)
+BACKGROUND_COLOR = (255,255,255)
 
-# Main loop
+
+
+
+# Main loop ::::::::::::::::::::::::::::::::::::::::::::::::::::
 
 while True:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT():
+        if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
 
-    screen.fill(background_color)
+    screen.fill(BACKGROUND_COLOR)
 
     pygame.display.update()
     clock.tick(60)
+    
